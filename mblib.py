3004bf346a88df4f556673d7fcb67afb147f44d2
Francisco Melicias <melicias1999@gmail.com>
2023-03-10 12:40:07 +0000

    Added calculateMetrics function to calculate all the metrics needed

--- mblib.py
+++ mblib.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import balanced_accuracy_score
 #from pprint import pprint
-#import numpy as np
+
 
 def displayInformationDataFrame(df,showCategoricals = False, showDetailsOnCategorical = False, showFullDetails = False):
     """
@@ -27,6 +35,8 @@ def displayInformationDataFrame(df,showCategoricals = False, showDetailsOnCatego
     --------
     None
     """
+    
+    print(f"---\nLines: {len(df.index)}\nColumns: {len(df.columns)} \n---")
     
     # Create an empty summary DataFrame with columns for data type, column name, and unique values.
     summary_df = pd.DataFrame(columns=['Data Type', 'Column Name', 'Unique Values'])
@@ -78,3 +88,49 @@ def displayInformationDataFrame(df,showCategoricals = False, showDetailsOnCatego
     # Display the summary DataFrame.
     return display(summary_df)
 
+
+
+
+def calculateMetrics(modelName, yTrue, yPred, average='binary'):
+    """
+    Calculate and print the performance metrics of a classification model.
+    
+    Parameters:
+    modelName (str): The name of the classification model.
+    yTrue (array-like): The true labels.
+    yPred (array-like): The predicted labels.
+    average (str or None, optional): The averaging method to use for multi-class classification. One of 
+        {'micro', 'macro', 'weighted', 'binary'} or None (default: 'binary'). If None, only binary 
+        classification metrics will be computed.
+    
+    Returns:
+    None
+    
+    Raises:
+    ValueError: If `average` is not one of {'micro', 'macro', 'weighted', 'binary'} or None.
+    
+    """
+    # Check if average parameter is valid
+    if average != 'micro' and average != 'macro' and average != 'weighted' and average != 'binary' and average != None:
+        print("Average must be one of this options: {‘micro’, ‘macro’, ‘samples’, ‘weighted’, ‘binary’} or None, default=’binary’")
+        return
+    
+    # Print the name of the model and calculate accuracy and precision
+    print(f"--- Performance of {modelName} ---")
+    acc = accuracy_score(y_true = yTrue, y_pred = yPred)
+    precision = precision_score(y_true = yTrue, y_pred = yPred, average = average)
+    print(f'Accuracy : {np.round(acc*100,2)}%\nPrecision: {np.round(precision*100,2)}%')
+    
+    # Calculate and print recall and F1-score
+    f1 = f1_score(y_true = yTrue, y_pred = yPred, average = average)
+    recall = recall_score(y_true = yTrue, y_pred = yPred, average = average)
+    print(f'Recall: {np.round(recall*100,2)}%\nF1-score: {np.round(f1*100,2)}%')
+    
+    #auc_sklearn = roc_auc_score(y_true = yTrue, y_score = yPred, average = average)
+    #print(f'Roc auc: {np.round(auc_sklearn*100,2)}%')
+    
+    # Calculate and print balanced accuracy and classification report
+    print(f"Balanced accuracy: {np.round(balanced_accuracy_score(yTrue, yPred)*100,2)}%")
+    print(f"Classification report:\n{classification_report(yTrue, yPred)}")
+    
+    
