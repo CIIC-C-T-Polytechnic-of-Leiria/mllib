a0bcc1bd5497603ca45449e1992bc62fe6fe08cf
Francisco Melicias <melicias1999@gmail.com>
2023-03-14 12:07:09 +0000

    updated so this can be a library

---
+++ README.md
@@ -0,0 +1,5 @@
+install this library
+
+python setup.py bdist_wheel
+
+pip install dist/mlciic-0.1.0-py3-none-any.whl 
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+setup(
+    name='mlciic',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Library to help with machine learning projects at CIIC C&T',
+    author='CIIC C&T',
+    license='MIT',
+)
+"""
+setup(
+    name='mypythonlib',
+    packages=find_packages(include=['mypythonlib']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Me',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
+"""
