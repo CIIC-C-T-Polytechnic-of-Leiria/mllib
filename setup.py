aa93404e3b2bbccfae483c6015f04a3ae4d3ee84
Francisco Melicias <melicias1999@gmail.com>
2023-03-24 13:50:32 +0000

    change realese number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='mlciic',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0',
     description='Library to help with machine learning projects at CIIC C&T',
     author='CIIC C&T',
     license='MIT',
